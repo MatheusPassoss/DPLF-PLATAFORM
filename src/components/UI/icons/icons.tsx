3e66d3bd5c6aade8375ef079d0bd351c2be1588e
Vinicius Costa <SkinzinSTG_@hotmail.com>
2024-02-29 02:23:06 -0300

    REFACTOR: Mudança de exportação de Icones de maneira geral

--- src/components/UI/icons/icons.tsx
+++ src/components/UI/icons/icons.tsx
@@ -1,5 +1,3 @@
 "use client"; 
 
-import { BookOpenText, Student, UsersFour, ChalkboardTeacher, CopySimple, Question, InstagramLogo, LinkedinLogo, List, X, SignIn, Target, Copy, CaretRight, CaretLeft, CaretDown, MapPin, GraduationCap, BagSimple, Buildings, Handshake, PresentationChart, FlagPennant, CalendarBlank, UsersThree, UserCircle  } from "@phosphor-icons/react";
-
-export { BookOpenText, Student, UsersFour, ChalkboardTeacher, CopySimple, Question, InstagramLogo, LinkedinLogo, List, X, SignIn, Target, Copy, CaretRight, CaretLeft, CaretDown, MapPin, GraduationCap, BagSimple, Buildings, Handshake, PresentationChart, FlagPennant, CalendarBlank, UsersThree, UserCircle } 
+export * from "@phosphor-icons/react";
